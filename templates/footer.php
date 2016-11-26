a620b143e174a745e4e7835d3cef0528f92cba12
Szabo Gabor <szabogabi@gmail.com>
2016-11-26 23:07:05 +0100

    jo

--- templates/footer.php
+++ templates/footer.php
@@ -1,4 +1,5 @@
-<div class="row ps ps--narrow">
+<div class="ps ps--accent ps--narrow">
+<div class="row">
   <div class="columns medium-8">
     <h3>Kapcsolatfelvétel</h3>
     <p>Bármilyen kérdésük van, az alábbi e-mail címen várjuk jelentkezésüket: <a href="mailto:haris@haris.hu">haris@haris.hu</a></p>
@@ -10,7 +11,8 @@
     Városmajor utca 48/b</p>
   </div>
 </div>
-<footer class="sitefooter ps ps--accent">
+</div>
+<footer class="sitefooter ps ps--dark">
   <div class="row wow fadeInUp">
     <div class="columns">
       <?php dynamic_sidebar('sidebar-footer'); ?>
